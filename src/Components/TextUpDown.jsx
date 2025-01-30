af68fc6d5efbd2cc0911d5d51a7cfc5895cf609a
kaneki-desu <sibajit.mazumdar@gmail.com>
2025-01-30 19:01:29 +0530

    TextUpDownCenter

--- src/Components/TextUpDown.jsx
+++ src/Components/TextUpDown.jsx
@@ -3,7 +3,7 @@ import '../css/TextUpDown.css'
 import { BadgeIndianRupee } from 'lucide-react';
 const TextUpDown = () => {
   return (
-    <div className='relative h-screen w-screen flex-col flex ' style={{paddingLeft:"12vw", paddingTop:"15vw"}}>
+    <div className='relative h-screen w-screen flex-col flex mobile:justify-center' style={{paddingLeft:"12vw", paddingTop:"15vw"}}>
         <ul className='mobile:hidden mobile:absolute inner-heading -z-1 '>
             <div >
             Create<br/>
