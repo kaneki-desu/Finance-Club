1cf326acb8b233c22688752dc5eb6db63c07151c
kaneki-desu <sibajit.mazumdar@gmail.com>
2025-02-01 12:19:46 +0530

    Gallery commit 2

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -90,7 +90,7 @@ export default function TimelineStep() {
   return (
     <div className="pc:h-96" >
       {/* <h1>react-scroll-animation component</h1> */}
-      <div className={`  mobile:absolute mobile:hidden stub1 ` }>.<p className="pc:absolute pc:hidden"> Quotes of the day. </p></div>
+      <div className={`  mobile:absolute mobile:hidden stub1 text-black` }>.<p className="pc:absolute pc:hidden"> Quotes of the day. </p></div>
       <div className={`  pc:absolute pc:hidden Mstub1 ` }><p> Quotes of the day</p></div>
       <TimelineObserver
         initialColor="#ff05057e"
